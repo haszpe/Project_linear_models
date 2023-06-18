fe110b94a73772a06bac766469a9eb93af77e24f
Zygmunt Łatyszewicz <88512931+zgmnt@users.noreply.github.com>
2023-06-18 21:12:15 +0200

    if else

--- Projekt_modele_1.R
+++ Projekt_modele_1.R
@@ -11,11 +11,11 @@ source("~/Documents/GitHub/Project_linear_models/functions.R")
 #----PROGRAM--------------------------------------------------------------------
 
 #Wczytanie danych:
-'Wczytanie pliku z danymi ze ścieżki podanej przez użytkownika  -
-     > zdefiniować w dokumentacji format pliku csv'
+'Wczytanie pliku z danymi ze Ĺ›cieĹĽki podanej przez uĹĽytkownika  -
+     > zdefiniowaÄ‡ w dokumentacji format pliku csv'
 
-data <- readline(prompt = "Podaj ścieżkę do pliku: ")
-# przyk�adowe dane:
+data <- readline(prompt = "Podaj Ĺ›cieĹĽkÄ™ do pliku: ")
+# przykładowe dane:
 # http://theta.edu.pl/wp-content/uploads/2012/02/DanePakietyStatystyczne2.csv
 data <- read.csv2(dta)
 
@@ -24,52 +24,65 @@ check(data)
 
 #----WYBOR PRZEPROWADZANEJ ANALIZY----------------------------------------------
 
-"Definiujemy zmienną funk, która określa jaka funkcja zostanie wykorzystana w
+"Definiujemy zmiennÄ… funk, ktĂłra okreĹ›la jaka funkcja zostanie wykorzystana w
 naszym programie do przeprowadzenia danej analizy"
+
+
+
+
+
+
+
+
 
 analiza <- readline(prompt = "Jaką analizę chcesz przeprowadzić? \n
                           Do wyboru masz: T-student, regresja, ANOVA")
 
+  
 if (analiza == 'T-student') {
+  print("robie t.test")
+  
   t_stud <- readline(prompt = "Jaki test T-studenta chcesz przepowadzić?
                               Masz do wyboru: one-sample, two-sample independent, 
                               two-sample dependent")
+   
+   proba <- readline(prompt = 'JakÄ… zmiennÄ… chcesz przetestowaÄ‡?')
+   if_norm
+   mo <- readline(prompt = 'Testowana Ĺ›rednia:')
+   alt <-readline(prompt = 'Alternatywa:')
+   t_test_jedna_niezal(proba, mo, alt)
   
   if (t_stud == 'one-sample') {
-    proba <- readline(prompt = 'Jaką zmienną chcesz przetestować?')
-    if_norm
-    mo <- readline(prompt = 'Testowana średnia:')
-    alt <-readline(prompt = 'Alternatywa:')
-    t_test_jedna_niezal(proba, mo, alt)
-  } 
-  if (t_stud =='two-sample independent'){
-    proba_1 <-readline(prompt = 'Próba pierwsza:')
-    proba_2 <- readline(prompt = 'Próba druga:')
+    print('one-sample')
+  } else if (t_stud =='two-sample independent'){
+    print("two-sample independent")
+    proba_1 <-readline(prompt = 'PrĂłba pierwsza:')
+    proba_2 <- readline(prompt = 'PrĂłba druga:')
     alt <-readline(prompt = 'Alternatywa: (greater, lesser, none')
     t_test_dwie_niezal(proba_1, proba_2, alt)
-  }
-  if (t_stud == 'two-sample dependent'){
-    proba_1 <-readline(prompt = 'Próba pierwsza:')
-    proba_2 <- readline(prompt = 'Próba druga:')
+  } else {
+    print('two-sample dependent')
+    proba_1 <-readline(prompt = 'PrĂłba pierwsza:')
+    proba_2 <- readline(prompt = 'PrĂłba druga:')
     alt <-readline(prompt = 'Alternatywa: (greater, lesser, none')
     t_test_dwie_zal(proba_1, proba_2, alt)
-  }}
-  
-if (analiza == 'regresja') {
-  zalezna <- readline(prompt = "Jaka kolumna z pliku wejściowego jest zmienną zależną?
-                              *Zmienna musi być numeryczna")
+  }
+} else if (analiza == 'regresja') {
+  print("wykonuje regresje")
+  zalezna <- readline(prompt = "Jaka kolumna z pliku wejĹ›ciowego jest zmiennÄ… zaleĹĽnÄ…?
+                              *Zmienna musi byÄ‡ numeryczna")
   
   niezalezne <- readline(prompt = "Jakie kolumny sa zmiennymi niezaleznymi?
-                          Wpisz nazwy kolumn bez cudzysłowiów i po przecinkach")
+                          Wpisz nazwy kolumn bez cudzysĹ‚owiĂłw i po przecinkach")
   niezalezne <- unlist(strsplit(niezalezne, ", "))
   
   regresja(niezalezne, zalezne)
+} else {
+  print("wykonuje anova")
 }
-  
-if (analiza == 'ANOVA') {
-  funk <- ANOVA}
 
-#----ZAPISANIE WYNIK�W----------------------------------------------------------
+
+#----ZAPISANIE WYNIKÓW----------------------------------------------------------
 
 #Czy chcesz zapisac wyniki? 
   #if ('y') write(wynik) to csv
