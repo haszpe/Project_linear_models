1d1f039ea545933729c75ef69532e8deef7d56ef
KrzychuBomba <107877786+KrzychuBomba@users.noreply.github.com>
2023-06-20 17:18:20 +0200

    Update Projekt_modele_1.R

--- Projekt_modele_1.R
+++ Projekt_modele_1.R
@@ -27,34 +27,39 @@ c <- rnorm(20, 25, 3)
 
 #----WYBOR PRZEPROWADZANEJ ANALIZY----------------------------------------------
 
-"Definiujemy zmienna funk, ktora okresla jaka funkcja zostanie wykorzystana w
-naszym programie do przeprowadzenia danej analizy"
+#Definiujemy zmienna funk, ktora okresla jaka funkcja zostanie wykorzystana w
+#naszym programie do przeprowadzenia danej analizy
 
-analiza <- as.integer(readline(prompt = "Jaka analize chcesz przeprowadzic? \n
-                          Do wyboru masz: (1)T-student, (2)regresja, (3)ANOVA. "))
+analiza <- as.integer(readline(prompt = "Jaka analize chcesz przeprowadzic?   
+ Do wyboru masz:
+ (1)T-student,
+ (2)regresja,
+ (3)ANOVA, \n"))
 
 while (TRUE) {
   if(analiza == 1) {
     print("Przeprowadzam test T-studenta...")
     
-    t_stud <-  as.integer(readline(prompt = "Jaki test T-studenta chcesz przepowadzic? \n
-                              Masz do wyboru: (1)one-sample, (2)two-sample independent, 
-                              (3)two-sample dependent. "))
+    t_stud <-  as.integer(readline(prompt = "Jaki test T-studenta chcesz przepowadzic?   
+  Masz do wyboru:
+  (1)one-sample,
+  (2)two-sample independent,
+  (3)two-sample dependent, \n"))
     
     if(t_stud == 1) {
       print('...dla jednej proby.')
-      proba <- readline(prompt = 'Jaka zmienna chcesz przetestowac? ')
+      proba <- readline(prompt = 'Jaka zmienna chcesz przetestowac?   ')
       if_num(proba)
       if_norm(proba)
-      mo <- readline(prompt = 'Testowana srednia:')
-      alt <-readline(prompt = 'Alternatywa? (greater/lesser/none) ')
+      mo <- readline(prompt = 'Testowana srednia:   ')
+      alt <-readline(prompt = 'Alternatywa? (greater/lesser/none)   ')
       t_test_jedna_niezal(proba, mo, alt)
       break
       
     } else if(t_stud ==2){
-      print("two-sample independent")
-      proba_1 <-readline(prompt = 'Proba pierwsza: ')
-      proba_2 <- readline(prompt = 'Proba druga: ')
+      print("...dla dwoch prob niezaleznych.")
+      proba_1 <-readline(prompt = 'Proba pierwsza:   ')
+      proba_2 <- readline(prompt = 'Proba druga:   ')
       
       if_num(proba_1)
       if_norm(proba_1)
@@ -63,14 +68,14 @@ while (TRUE) {
       
       homo_var(proba_1, proba_2)
       
-      alt <-readline(prompt = 'Alternatywa? (greater/lesser/none) ')
+      alt <-readline(prompt = 'Alternatywa? (greater/lesser/none)   ')
       t_test_dwie_niezal(proba_1, proba_2, alt)
       break
       
     } else {
       print('...dla dwoch prob zaleznych.')
-      proba_1 <-readline(prompt = 'Proba pierwsza: ')
-      proba_2 <- readline(prompt = 'Proba druga: ')
+      proba_1 <-readline(prompt = 'Proba pierwsza:   ')
+      proba_2 <- readline(prompt = 'Proba druga:   ')
       
       if_num(proba_1)
       if_norm(proba_1)
@@ -78,7 +83,7 @@ while (TRUE) {
       if_norm(proba_2)
       homo_var(proba_1, proba_2)
       
-      alt <-readline(prompt = 'Alternatywa? (greater/lesser/none) ')
+      alt <-readline(prompt = 'Alternatywa? (greater/lesser/none)   ')
       t_test_dwie_zal(proba_1, proba_2, alt)
       break
       
@@ -86,14 +91,14 @@ while (TRUE) {
   } else if (analiza == 2) {
     print("Przeprowadzam regresje.")
     columns <- colnames(data)
-    zalezna <- readline(prompt = "Jaka kolumna z pliku wejsciowego jest zmienna zalezna?
-                              (zmienna musi byc numeryczna) ")
+    zalezna <- readline(prompt = "Jaka kolumna z pliku wejsciowego jest zmienna zalezna?   
+    (zmienna musi byc numeryczna) ")
     
     zalezna <- check_var_name(zalezna, columns)
     
     
-    niezalezne <- readline(prompt = "Jakie kolumny sa zmiennymi niezaleznymi?
-                          (wpisz nazwy kolumn bez cudzyslwowu i po przecinkach) ")
+    niezalezne <- readline(prompt = "Jakie kolumny sa zmiennymi niezaleznymi?   
+    (wpisz nazwy kolumn bez cudzyslwowu i po przecinkach)")
     niezalezne <- unlist(strsplit(niezalezne, ", "))
     
     niezalezne <- check_var_name(niezalezne, columns)
@@ -107,14 +112,20 @@ while (TRUE) {
     columns <- colnames(data)
     
     # ustalenie zmiennych liczbowej i grupujacej:
-    zmienna_liczbowa <- readline(prompt = "Podaj nazwe kolumny zawierajaca dane liczbowe 
-                                 (wpisz nazwe kolumny bez cudzyslowu) ")
+    zmienna_liczbowa <- readline(prompt = "Podaj nazwe kolumny zawierajaca dane liczbowe:   
+    (wpisz nazwe kolumny bez cudzyslowu)")
     zmienna_liczbowa <- check_var_name(zmienna_liczbowa, columns)
+    #if_num(zmienna_liczbowa)
+    #if_norm(zmienna_liczbowa)
     
-    zmienna_grupujaca <- readline(prompt = "Podaj kolumne grupujaca
-                                  (wpisz nazwe kolumny bez cudzyslowu) ")
+    zmienna_grupujaca <- readline(prompt = "Podaj kolumne grupujaca:   
+    (wpisz nazwe kolumny bez cudzyslowu)")
     zmienna_grupujaca <- unlist(strsplit(zmienna_grupujaca, ", "))
     zmienna_grupujaca <- check_var_name(zmienna_grupujaca, columns)
+    #if_num(zmienna_grupujaca)
+    #if_norm(zmienna_grupujaca)
+    
+    #homo_var(zmienna_liczbowa, zmienna_grupujaca)
     
     # wywolanie ANOVY:
     wynik <- ANOVA(as.matrix(data[zmienna_liczbowa]),as.matrix(data[zmienna_grupujaca]))
@@ -122,7 +133,7 @@ while (TRUE) {
     
     # opcja wykonania testu post-hoc:
     if (p_value < 0.05){
-       Tuk <- realine(prompt = "Czy chcesz wykonac test Tukeya HSD? (y/n) ")
+       Tuk <- realine(prompt = "Czy chcesz wykonac test Tukeya HSD? (y/n)   ")
        if (Tuk == y){
          #post_hoc()
        }
@@ -135,7 +146,7 @@ while (TRUE) {
   } else{
     print(paste("Bledny numer analizy.", analiza))
     print("Do wyboru masz: (1)T-student, (2)regresja, (3)ANOVA")
-    analiza <-  as.integer(readline(prompt = "Jaka analize chcesz przeprowadzic? "))
+    analiza <-  as.integer(readline(prompt = "Jaka analize chcesz przeprowadzic?   "))
   }
 }
 print("Koniec analizy.")
