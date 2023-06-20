ccd3dd0e6360c30e1da4abe98fd490388605e009
Hanna Pęciak <103579210+haszpe@users.noreply.github.com>
2023-06-20 18:20:46 +0200

    run logic

--- Projekt_modele_2.R
+++ Projekt_modele_2.R
@@ -9,28 +9,29 @@ Zygmunt Latyszewicz   121724'
 
 print("Do wyboru masz: (1) efekty modelu, (2)parametry wariancji, (3)oba")
 wybor <-  as.integer(readline(prompt = ""))
+run <- TRUE
 
-
-while (TRUE) {
+while (run == TRUE) {
   if(wybor == 1) {
     print("Przeprowadzam efekty modelu ")
-    break
+    run <-FALSE
       
   } else if (wybor == 2) {
     print("Przeprowadzam regresje ")
-    break
+    run <-FALSE
     
   } else if (analiza == 3) {
     print("oba elementy ")
-    break
+    run <-FALSE
     
   } else{
     print(paste("Bledny wybor", wybor))
     print("Do wyboru masz: (1) efekty modelu, (2)parametry wariancji, (3)oba")
     wybor <-  as.integer(readline(prompt = "co chcesz otrzymac? "))
+    break
   }
 }
-
+print('Koniec programu.')
 
 #----FUNKCJE------------------------------------------------------------------
 mme = function(y, X, Z, A, sigma_a, sigma_e) {
